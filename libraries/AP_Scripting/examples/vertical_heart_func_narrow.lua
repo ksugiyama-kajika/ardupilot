ec782af659a79b0edb8deecff2b615f4e6b4119e
kentaro <kajikagarden.2020@gmail.com>
2021-12-29 15:00:14 +0900

    remove unnecessary comments in vertical_heart.lua

--- libraries/AP_Scripting/examples/vertical_heart_func_narrow.lua
+++ libraries/AP_Scripting/examples/vertical_heart_func_narrow.lua
@@ -1,10 +1,10 @@
--- command a Copter to takeoff to 5m and fly a vertical circle in the clockwise direction
+-- command a Copter to takeoff to 10m and fly a vertical heart in the clockwise direction
 --
 -- CAUTION: This script only works for Copter
 -- this script waits for the vehicle to be armed and RC6 input > 1800 and then:
 --    a) switches to Guided mode
---    b) takeoff to 5m
---    c) flies a vertical circle using the velocity controller
+--    b) takeoff to 10m
+--    c) flies a vertical heart using the velocity controller
 --    d) switches to RTL mode
 
 local takeoff_alt_above_home = 10
@@ -20,6 +20,9 @@ local yaw_sin = 0                   -- sine of yaw at takeoff
 -- heart_function is ...
 -- x = 2*1*sin(t)^3
 -- y = 2*(1*cos(t)-cos(t)^4)
+-- heart_velocity is ...
+-- Vx = dx/dt 
+-- Vy = dy/dt
 
 -- the main update function that uses the takeoff and velocity controllers to fly a rough square pattern
 function update()
@@ -51,7 +54,7 @@ function update()
             bottom_left_loc = curr_loc          -- record location when starting square
           end
         end
-      elseif (stage == 3 ) then   -- Stage3: fly a vertical circle
+      elseif (stage == 3 ) then   -- Stage3: fly a vertical heart
 
         -- calculate velocity vector
         circle_angle = circle_angle + circle_angle_increment
@@ -60,20 +63,17 @@ function update()
         end
         local target_vel = Vector3f()
 
-        -- conversion to 1 m/s by dividing (Vx^2 + Vy^2)^(1/2)
+        -- convert to 1 m/s by dividing (Vx^2 + Vy^2)^(1/2)
         local speed_converter = max_speed / ((-48 * (math.sin(math.rad(circle_angle))^2) 
                 * math.cos(math.rad(circle_angle)))^2 + (-1 * (-13 * math.sin(math.rad(circle_angle))
                 + 10 * math.sin(math.rad(2 * circle_angle)) + 6 * math.sin(math.rad(3 * circle_angle))
                 + 4 * math.sin(math.rad(4 * circle_angle))))^2)^(1/2)
-
-        -- local vel_xy = math.cos(math.rad(circle_angle)) * circle_speed
-        -- local vel_xy = 2 * math.sin(math.rad(circle_angle)) * (2 * (math.cos(math.rad(circle_angle)))^3 - 1)
+      
         local vel_xy = speed_converter * (-48 * (math.sin(math.rad(circle_angle))^2) * math.cos(math.rad(circle_angle)))
         target_vel:x(yaw_sin * vel_xy)
         target_vel:y(yaw_cos * -vel_xy)
-        -- target_vel:z(-math.sin(math.rad(circle_angle)) * circle_speed)
-        -- target_vel:z(48 * (math.sin(math.rad(circle_angle))^2) * math.cos(math.rad(circle_angle)))
         target_vel:z(speed_converter * (-1 * (-13 * math.sin(math.rad(circle_angle)) + 10 * math.sin(math.rad(2 * circle_angle)) + 6 * math.sin(math.rad(3 * circle_angle)) + 4 * math.sin(math.rad(4 * circle_angle)))))
+
         -- send velocity request
         if not (vehicle:set_target_velocity_NED(target_vel)) then
           gcs:send_text(0, "failed to execute velocity command")
