32290fe4de51df25a9e718f16d2c14fd22d3ca04
kentaro <kajikagarden.2020@gmail.com>
2022-01-13 23:24:02 +0900

    update vertical_heart_2

--- libraries/AP_Scripting/examples/vertical_heart_func_narrow2.lua
+++ libraries/AP_Scripting/examples/vertical_heart_func_narrow2.lua
@@ -11,7 +11,7 @@ local takeoff_alt_above_home = 10
 local copter_guided_mode_num = 4
 local copter_rtl_mode_num = 6
 local stage = 0
-local circle_angle = 175            -- initial deg is 175deg to set point on bottom of the heart
+local circle_angle = 175           -- initial deg is 175deg to set point on bottom of the heart
 local circle_angle_increment = 0.5  -- increment the target angle by 1 => 0.5 deg every 0.1 sec (i.e. 10 => 5deg/sec)
 local max_speed = 1                 -- max velocity is 1m/s
 local yaw_cos = 0                   -- cosine of yaw at takeoff
@@ -19,16 +19,22 @@ local yaw_sin = 0                   -- sine of yaw at takeoff
 
 -- heart_function is ...
 -- x = 2*1*sin(t)^3
--- y = 2*(1*cos(t)-cos(t)^4)
+-- y = 13cos(t)-5cos(2t)-2cos(3t)-cos(4t)
+-- heart No.2
+-- x = sin(t)^3
+-- y = cos(t) - cos(2t)/3 -cos(3t)/6
+-- heart No.3
+-- x = 16 * sin(t)^3
+-- y = 13 * cos(t) - 5 * cos(2t) - 2 * cos(3t)
 -- heart_velocity is ...
--- Vx = dx/dt 
+-- Vx = dx/dt
 -- Vy = dy/dt
 
 -- the main update function that uses the takeoff and velocity controllers to fly a rough square pattern
 function update()
   if not arming:is_armed() then -- reset state when disarmed
     stage = 0
-    circle_angle = 175
+    circle_angle = 180
   else
     pwm6 = rc:get_pwm(6)
     if pwm6 and pwm6 > 1800 then    -- check if RC6 input has moved high
@@ -55,16 +61,38 @@ function update()
           end
         end
       elseif (stage == 3 ) then   -- Stage3: fly a vertical heart
-
         -- calculate velocity vector
-        circle_angle = circle_angle + circle_angle_increment
+        -- circle_angle = circle_angle + circle_angle_increment
         if (circle_angle >= 545) then
           stage = stage + 1
         end
+        pwm9 = rc:get_pwm(9)
+        if pwm9 == nil or pwm9 < 1400 then
+          gcs:send_text(0, "rc9:narrow=>2000, wide=>1500")
+        end
         local target_vel = Vector3f()       
-
         -- convert to 1 m/s by dividing (Vx^2 + Vy^2)^(1/2)
-        local speed_converter = max_speed / ((-48 * (math.sin(math.rad(circle_angle))^2) 
+        -- heart No.1 narrow
+        if pwm9 and pwm9 >= 1400 and pwm9 <= 1600 then
+          gcs:send_text(0, "drawing a wide heart")
+          circle_angle = circle_angle + circle_angle_increment
+          local speed_converter = max_speed / ((-48 * (math.sin(math.rad(circle_angle))^2) 
+                * math.cos(math.rad(circle_angle)))^2 + (-1 * (-13 * math.sin(math.rad(circle_angle))
+                + 10 * math.sin(math.rad(2 * circle_angle)) + 6 * math.sin(math.rad(3 * circle_angle))
+                + 4 * math.sin(math.rad(4 * circle_angle))))^2)^(1/2)
+          local vel_xy = speed_converter * (-48 * (math.sin(math.rad(circle_angle))^2) * math.cos(math.rad(circle_angle)))
+          target_vel:x(yaw_sin * vel_xy)
+          target_vel:y(yaw_cos * -vel_xy)
+          target_vel:z(speed_converter * (-1 * (-13 * math.sin(math.rad(circle_angle)) + 10 * math.sin(math.rad(2 * circle_angle)) + 6 * math.sin(math.rad(3 * circle_angle)) + 4 * math.sin(math.rad(4 * circle_angle)))))
+          -- send velocity request
+          if not (vehicle:set_target_velocity_NED(target_vel)) then
+          gcs:send_text(0, "failed to execute velocity command")
+        end
+        -- heart No.2 wide
+        elseif pwm9 and pwm9 >= 1800 then
+          gcs:send_text(0, "drawing a narrow heart")
+          circle_angle = circle_angle + circle_angle_increment
+          local speed_converter = max_speed / ((-48 * (math.sin(math.rad(circle_angle))^2) 
                 * math.cos(math.rad(circle_angle)))^2 + (-1 * (-13 * math.sin(math.rad(circle_angle))
                 + 10 * math.sin(math.rad(2 * circle_angle)) + 6 * math.sin(math.rad(3 * circle_angle))
                 + 4 * math.sin(math.rad(4 * circle_angle))))^2)^(1/2)
@@ -73,10 +101,10 @@ function update()
         target_vel:x(yaw_sin * vel_xy)
         target_vel:y(yaw_cos * -vel_xy)
         target_vel:z(speed_converter * (-1 * (-13 * math.sin(math.rad(circle_angle)) + 10 * math.sin(math.rad(2 * circle_angle)) + 6 * math.sin(math.rad(3 * circle_angle)) + 4 * math.sin(math.rad(4 * circle_angle)))))
-
-        -- send velocity request
-        if not (vehicle:set_target_velocity_NED(target_vel)) then
+          -- send velocity request
+          if not (vehicle:set_target_velocity_NED(target_vel)) then
           gcs:send_text(0, "failed to execute velocity command")
+        end
         end
       elseif (stage == 4) then  -- Stage4: change to RTL mode
         vehicle:set_mode(copter_rtl_mode_num)
